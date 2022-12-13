562e477ed21be7bbaefe6dd9a85bf2c75676b8de
aawad1 <aawad1@etf.unsa.ba>
2022-12-13 15:28:37 +0100

    napravio osnovne klase

---
+++ tut6/rpr-t6/src/main/java/dao/DAO.java
@@ -0,0 +1,10 @@
+package dao;
+import java.util.List;
+
+public interface DAO<T> {
+    T getById(int id);
+    T add(T item);
+    T update(T item);
+    void delete(int id);
+    List<T> getAll();
+}
